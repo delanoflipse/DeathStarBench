982059ca40e369eece0cd3574733449c2f3ad6ef
Delano Flipse <delanoflipse@hotmail.com>
2025-07-03 17:12:05 +0200

    feat: update tracer, add hints and config to match fit config

--- hotelReservation/tracing/tracer.go
+++ hotelReservation/tracing/tracer.go
@@ -5,43 +5,21 @@ import (
 	"errors"
 	"log"
 
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 )
 
-// var (
-// 	defaultSampleRatio float64 = 0.01
-// )
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
 
-// Init returns a newly configured tracer
-func Init(serviceName, host string) (*trace.TracerProvider, error) {
-
-	// ratio := defaultSampleRatio
-	// if val, ok := os.LookupEnv("JAEGER_SAMPLE_RATIO"); ok {
-	// 	ratio, _ = strconv.ParseFloat(val, 64)
-	// 	if ratio > 1 {
-	// 		ratio = 1.0
-	// 	}
-	// }
-
-	// log.Info().Msgf("Jaeger client: adjusted sample ratio %f", ratio)
-	// tempCfg := &config.Configuration{
-	// 	ServiceName: serviceName,
-	// 	Sampler: &config.SamplerConfig{
-	// 		Type:  "probabilistic",
-	// 		Param: ratio,
-	// 	},
-	// 	Reporter: &config.ReporterConfig{
-	// 		LogSpans:            false,
-	// 		BufferFlushInterval: 1 * time.Second,
-	// 		LocalAgentHostPort:  host,
-	// 	},
-	// }
-
-	// log.Info().Msg("Overriding Jaeger config with env variables")
-	// cfg, err := tempCfg.FromEnv()
-
+func newTracerProvider() (*trace.TracerProvider, error) {
 	ctx := context.Background()
 	traceExporter, err := otlptracegrpc.New(ctx)
 	if err != nil {
@@ -69,6 +47,22 @@ func Init(serviceName, host string) (*trace.TracerProvider, error) {
 		trace.WithSyncer(traceExporter),
 		trace.WithSampler(trace.AlwaysSample()),
 	)
+	return tracerProvider, nil
+}
 
+// Init returns a newly configured tracer
+func Init(serviceName, host string) (*trace.TracerProvider, error) {
+	// Set up propagator.
+	prop := newPropagator()
+	otel.SetTextMapPropagator(prop)
+
+	// Set up trace provider.
+	tracerProvider, tracerErr := newTracerProvider()
+	if tracerErr != nil {
+		log.Printf("Failed to create tracer provider: %v", tracerErr)
+		return nil, tracerErr
+	}
+
+	otel.SetTracerProvider(tracerProvider)
 	return tracerProvider, nil
 }
